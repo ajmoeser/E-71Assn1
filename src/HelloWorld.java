b2d94c9cadb593cb687414d51e6780ac2d2d3b3e
Alexander Moeser <amoeser@fas.harvard.edu>
2016-09-01 20:03:00 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/* Alexander Moeser
+ * CSCI E-71
+ * 8/31/2016
+ *
+ * Hello World class to demonstrate successful installation
+ * of IntelliJ IDEA. Takes name as command-line argument and
+ * prints "Hello, [name]!"
+ */
+
+
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
